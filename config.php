ff00580dd3779c4c6309f4483ff504dea2abd4db
Toshiyuki Takeda <support.alchemy@gmail.com>
2025-10-28 04:21:09 +0900

    コメントを分かり易く

--- config.php
+++ config.php
@@ -14,8 +14,11 @@ return [
         ],
         'hash_columns' => [
           'users' => [
-            'user_id_hash' => ['type' => 'hmac_sha256', 'normalize' => 'user_id'],
-            // normalizeはnone、email（余白削除で半角小文字）、number（余白記号を削除で半角数字）を選択
+            'user_id_hash' => ['type' => 'hmac_sha256', 'normalize' => ''],
+            // normalizeは以下の設定が可能
+            // 未設定（何もしない）
+            // email（余白削除で半角小文字）
+            // number（余白記号を削除で半角数字）
             'password'   => ['type' => 'argon2id'],
             // typeがhmac_sha256はpepper_keyが設定されるとハッシュが実行されるようになる
             // typoがArgon2idの場合はカラムが設定された段階でハッシュが実行されるようなる
